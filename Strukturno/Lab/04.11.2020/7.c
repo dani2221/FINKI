5cbee60df15562de3ba6a65944b6a2ac0a5e66c2
Danilo <danilo.najkov@gmail.com>
2020-11-05 18:21:12 +0100

    7ma bez nizi

--- Strukturno/Lab/04.11.2020/7.c
+++ Strukturno/Lab/04.11.2020/7.c
@@ -3,26 +3,18 @@
 // од вака внесените броеви.
 
 
-// neznam kako bez nizi???
 
 #include <stdio.h>
 
 int main(){
-    int n;
+    int n,maks=0,min=100;
     scanf("%d",&n);
-    int niza[n];
     for(int i=0;i<n;i++){
         int a;
         scanf("%d",&a);
-        niza[i]=a;
+        if(a>maks) maks = a;
+        if(a<min) min = a;
     }
-    int maksRazlika = 0;
-    for(int i=0;i<n-1;i++){
-        for(int j=1;j<n;j++){
-            if(niza[i]-niza[j]>maksRazlika) maksRazlika = niza[i]-niza[j];
-            if(niza[j]-niza[i]>maksRazlika) maksRazlika = niza[j]-niza[i];
-        }
-    }
-    printf("Najgolema razlika: %d",maksRazlika);
+    printf("Najgolema razlika: %d",maks-min);
     return 0;
 }
