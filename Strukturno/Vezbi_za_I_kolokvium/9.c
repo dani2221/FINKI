796a02dd9b561ceb7f9abaf527595c6c2dae3e6a
Danilo <danilo.najkov@gmail.com>
2020-11-10 23:00:49 +0100

    Podobruvanje na 9ta za vezbanje

--- Strukturno/Vezbi_za_I_kolokvium/9.c
+++ Strukturno/Vezbi_za_I_kolokvium/9.c
@@ -8,43 +8,23 @@
 #include <stdio.h>
 
 int main(){
-
-    int zbir = 0;
-    char dexCifra;
+    int suma=0;
     while(1){
-        scanf("%c",&dexCifra);
-        if(dexCifra=='.') break;
-        int broj;
-
-        if(dexCifra=='a' || dexCifra=='A') broj=10;
-        if(dexCifra=='b' || dexCifra=='B') broj=11;
-        if(dexCifra=='c' || dexCifra=='C') broj=12;
-        if(dexCifra=='d' || dexCifra=='D') broj=13;
-        if(dexCifra=='e' || dexCifra=='E') broj=14;
-        if(dexCifra=='f' || dexCifra=='F') broj=15;
-        if(dexCifra=='0') broj=0;
-        if(dexCifra=='1') broj=1;
-        if(dexCifra=='2') broj=2;
-        if(dexCifra=='3') broj=3;
-        if(dexCifra=='4') broj=4;
-        if(dexCifra=='5') broj=5;
-        if(dexCifra=='6') broj=6;
-        if(dexCifra=='7') broj=7;
-        if(dexCifra=='8') broj=8;
-        if(dexCifra=='9') broj=9;
-
-        zbir+=broj;
+        char hexBroj;
+        scanf("%c ",&hexBroj);
+        if(hexBroj=='.') break;
+        if(hexBroj>='0' && hexBroj<='9') suma+= hexBroj-'0';
+        if(hexBroj>='a' && hexBroj<='f') suma+= hexBroj-'a'+10;
+        if(hexBroj>='A' && hexBroj<='F') suma+= hexBroj-'A'+10;
     }
-    zbir/=2; //neznam zoshto zbirot e dupliran??? go sobira dva pati?
-    if(zbir%16==0){
-        int phZbir = zbir/10;
-        if(zbir%10==6 && phZbir%10==1){
+    if(suma%16==0){
+        if(suma%10==6 && suma/10%10==1){
             printf("Poln pogodok");
         }else{
             printf("Pogodok");
         }
     }else{
-        printf("%d",zbir);
+        printf("%d",suma);
     }
     return 0;
 }
