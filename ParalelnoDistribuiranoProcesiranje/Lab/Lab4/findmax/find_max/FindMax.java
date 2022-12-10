090abd24ad57ecec3e1769677dd91465b058552b
Danilo Najkov <danilo.najkov@gmail.com>
2022-12-10 11:06:10 +0100

    pidp d5

--- ParalelnoDistribuiranoProcesiranje/Lab/Lab4/findmax/find_max/FindMax.java
+++ ParalelnoDistribuiranoProcesiranje/Lab/Lab4/findmax/find_max/FindMax.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.*;
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
@@ -7,43 +8,55 @@ import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
 
 public class FindMax {
-    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
-        private Integer number = new Integer();
 
-        public void map(LongWritable key, Integer value, OutputCollector<IntWritable> output, Reporter reporter)
-                throws IOException {
-            number = value;
-            output.collect(number);
+    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken());
+                output.collect(new Text("max"), new IntWritable(Integer.parseInt(word.toString())));
+            }
         }
     }
 
     public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<IntWritable> output,
-                Reporter reporter) throws IOException {
-            int max = 0;
+
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            int max = Integer.MIN_VALUE;
+
             while (values.hasNext()) {
-                int newVal = values.next().get();
-                if(newVal>max){
-                    max = newVal;
+                int value = values.next().get();
+                if (value > max) {
+                    max = value;
                 }
             }
-            output.collect(max);
+
+            output.collect(new Text("max"), new IntWritable(max));
         }
     }
 
     public static void main(String[] args) throws Exception {
-        JobConf conf = new JobConf(WordCount.class);
-        conf.setJobName("findmax");
-        conf.setOutputKeyClass(Integer.class);
+        JobConf conf = new JobConf(MaxElement.class);
+        conf.setJobName("max_element");
+
+        conf.setOutputKeyClass(Text.class);
         conf.setOutputValueClass(IntWritable.class);
+
         conf.setMapperClass(Map.class);
-        conf.setCombinerClass(Reduce.class);
         conf.setReducerClass(Reduce.class);
+
         conf.setInputFormat(TextInputFormat.class);
         conf.setOutputFormat(TextOutputFormat.class);
+
         FileInputFormat.setInputPaths(conf, new Path(args[0]));
         FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
         JobClient.runJob(conf);
     }
 }
