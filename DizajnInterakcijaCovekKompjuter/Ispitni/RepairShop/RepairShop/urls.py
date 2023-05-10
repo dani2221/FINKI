e06da2d4594a34f36f17efac62ba40fc18a0af4c
Danilo <danilo.najkov@gmail.com>
2023-05-10 11:15:50 +0200

    dnick ispitna 2/3

--- DizajnInterakcijaCovekKompjuter/Ispitni/RepairShop/RepairShop/urls.py
+++ DizajnInterakcijaCovekKompjuter/Ispitni/RepairShop/RepairShop/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from CarRepair.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', index, name='index'),
 ]
